8afc3e031a63e7399071dc23366a6d31a8f7cd1e
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-07-15 18:47:58 +0000

    add all project files/folders

---
+++ models/index.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
+
+mongoose.Promise = Promise;
+
+module.exports.Todo = require("./todo");
